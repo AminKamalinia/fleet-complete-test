12c8faa31eb8aba1ecfcb976912a698d17146502
Amin Kamalinia <Amin.Kamalinia@eryaz.net>
2022-02-08 21:48:58 +0300

    vehiclePath

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -13,6 +13,8 @@ export class HomeComponent implements OnInit, OnDestroy {
   private subscription: Subscription;
   public selected: { startDate: moment.Moment, endDate: moment.Moment };
   //#region Google map
+  private map: any;
+  private vehiclePath: google.maps.Polyline;
   private markerCenter: google.maps.LatLngLiteral = { lat: 47.4073, lng: 7.76 };
   private mapOptions: google.maps.MapOptions = {
     mapTypeId: google.maps.MapTypeId.ROADMAP,
@@ -29,7 +31,7 @@ export class HomeComponent implements OnInit, OnDestroy {
   //#endregion
 
   public latestData: Array<any> = [];
-  public detail: any = [];
+  public detail: Array<any> = [];
   public selectedId?: number | null;
 
   constructor(private fleetCompleteService: FleetCompleteService) {
@@ -39,10 +41,12 @@ export class HomeComponent implements OnInit, OnDestroy {
       startDate: moment.utc(moment.now()).local().subtract(1, 'days'),
       endDate: moment.utc(moment.now()).local()
     };
+    this.map = null;
+    this.vehiclePath = new google.maps.Polyline();
   }
 
   ngOnInit(): void {
-    const map = new google.maps.Map(document.getElementById('map_driver') as HTMLElement, this.mapOptions);
+    this.map = new google.maps.Map(document.getElementById('map_driver') as HTMLElement, this.mapOptions);
 
     this.subscription = this.fleetCompleteService.keySubject.subscribe(() => {
       this.fleetCompleteService.getLastData().subscribe(result => {
@@ -54,7 +58,7 @@ export class HomeComponent implements OnInit, OnDestroy {
               lng: item.longitude
             },
             label: item.objectName,
-            map: map
+            map: this.map
           });
         });
       });
@@ -67,11 +71,35 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   public showDetail(): void {
     if (this.selectedId !== null && this.selectedId !== undefined) {
+      this.vehiclePath.setMap(null);
       this.fleetCompleteService.getRawData(
         this.selectedId ?? 0,
         moment.utc(this.selected.startDate).local().format('YYYY-MM-DD'),
         moment.utc(this.selected.endDate).local().format('YYYY-MM-DD')).subscribe(result => {
           this.detail = result.response;
+          let pathCoordinates: Array<google.maps.LatLng> = [];
+          this.detail.forEach(item => {
+            pathCoordinates.push(new google.maps.LatLng(item.Latitude, item.Longitude));
+          });
+
+          this.vehiclePath = new google.maps.Polyline({
+            path: pathCoordinates,
+            geodesic: true,
+            strokeColor: '#FF0000',
+            strokeOpacity: 1.0,
+            strokeWeight: 2,
+            icons: [{
+              icon: {
+                path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+                strokeColor: '',
+                fillOpacity: 1,
+                scale: 3,
+              },
+              repeat: '100px'
+            }]
+          });
+
+          this.vehiclePath.setMap(this.map);
         });
     } else {
       alert('Please select a row from top table');
